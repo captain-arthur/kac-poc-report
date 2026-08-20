eb6b7a076707e4c75d22fe0d2ccd28d17747d5ae
captain-arthur <rltk777@naver.com>
2026-08-20 20:04:27 +0900

    resu

--- k6/hello-rbac.js
+++ k6/hello-rbac.js
@@ -7,8 +7,9 @@ chai.config.aggregateChecks = false;
 
 const USER = 'kac-can-i';
 const CLUSTER_ADMIN = 't-rbac-kac-cluster-admin';
+const NS_ADMIN = 't-rbac-kac-ns-admin';
 const NS_VIEWER = 't-rbac-kac-ns-viewer';
-const NAMESPACE = 'rbac-test';
+const PAYMENTS = 'payments';
 
 const kubeconfig = yaml.load(open(__ENV.KUBECONFIG || `${__ENV.HOME}/.kube/config`));
 const context = kubeconfig.contexts.find((c) => c.name === kubeconfig['current-context']).context;
@@ -33,28 +34,46 @@ export const options = {
 export default function () {
   const catalog = discover();
 
-  describe(`as group ${CLUSTER_ADMIN} in namespace ${NAMESPACE}`, () => {
-    const actual = canI(catalog, CLUSTER_ADMIN, NAMESPACE);
+  describe(`--as-group=${CLUSTER_ADMIN}`, () => {
+    const actual = canI(catalog, CLUSTER_ADMIN, PAYMENTS);
 
     describe('Then', () => {
-      expect(actual[`can-i get pods -n ${NAMESPACE}`]).to.equal(`can-i get pods -n ${NAMESPACE} = yes`);
-      expect(actual[`can-i create pods -n ${NAMESPACE}`]).to.equal(`can-i create pods -n ${NAMESPACE} = yes`);
-      expect(actual[`can-i delete pods -n ${NAMESPACE}`]).to.equal(`can-i delete pods -n ${NAMESPACE} = no`);
-      expect(actual[`can-i create pods --subresource=exec -n ${NAMESPACE}`]).to.equal(
-        `can-i create pods --subresource=exec -n ${NAMESPACE} = yes`,
-      );
+      expect(actual['can-i * * -A']).to.equal('can-i * * -A = yes');
     });
   });
 
-  describe(`as group ${NS_VIEWER} in namespace ${NAMESPACE}`, () => {
-    const actual = canI(catalog, NS_VIEWER, NAMESPACE);
+  describe(`--as-group=${NS_ADMIN} -n ${PAYMENTS}`, () => {
+    const actual = canI(catalog, NS_ADMIN, PAYMENTS);
 
     describe('Then', () => {
-      expect(actual[`can-i get pods -n ${NAMESPACE}`]).to.equal(`can-i get pods -n ${NAMESPACE} = yes`);
-      expect(actual[`can-i create pods -n ${NAMESPACE}`]).to.equal(`can-i create pods -n ${NAMESPACE} = no`);
-      expect(actual[`can-i create pods --subresource=exec -n ${NAMESPACE}`]).to.equal(
-        `can-i create pods --subresource=exec -n ${NAMESPACE} = no`,
+      expect(actual[`can-i create pods --subresource=exec -n ${PAYMENTS}`]).to.equal(
+        `can-i create pods --subresource=exec -n ${PAYMENTS} = no`,
       );
+      expect(actual[`can-i create pods --subresource=portforward -n ${PAYMENTS}`]).to.equal(
+        `can-i create pods --subresource=portforward -n ${PAYMENTS} = no`,
+      );
+      expect(actual[`can-i get secrets -n ${PAYMENTS}`]).to.equal(`can-i get secrets -n ${PAYMENTS} = yes`);
+    });
+  });
+
+  describe(`--as-group=${NS_VIEWER} -n ${PAYMENTS}`, () => {
+    const actual = canI(catalog, NS_VIEWER, PAYMENTS);
+
+    describe('Then', () => {
+      expect(actual[`can-i create pods --subresource=exec -n ${PAYMENTS}`]).to.equal(
+        `can-i create pods --subresource=exec -n ${PAYMENTS} = no`,
+      );
+      expect(actual[`can-i create pods --subresource=portforward -n ${PAYMENTS}`]).to.equal(
+        `can-i create pods --subresource=portforward -n ${PAYMENTS} = no`,
+      );
+      expect(actual[`can-i get secrets -n ${PAYMENTS}`]).to.equal(`can-i get secrets -n ${PAYMENTS} = no`);
+      expect(actual[`can-i create pods -n ${PAYMENTS}`]).to.equal(`can-i create pods -n ${PAYMENTS} = no`);
+      expect(actual[`can-i delete pods -n ${PAYMENTS}`]).to.equal(`can-i delete pods -n ${PAYMENTS} = no`);
+      expect(actual[`can-i deletecollection pods -n ${PAYMENTS}`]).to.equal(
+        `can-i deletecollection pods -n ${PAYMENTS} = no`,
+      );
+      expect(actual[`can-i patch pods -n ${PAYMENTS}`]).to.equal(`can-i patch pods -n ${PAYMENTS} = no`);
+      expect(actual[`can-i update pods -n ${PAYMENTS}`]).to.equal(`can-i update pods -n ${PAYMENTS} = no`);
     });
   });
 }
@@ -98,10 +117,12 @@ function discover() {
 
 function canI(catalog, asGroup, namespace) {
   const actual = { 'can-i': '' };
-  console.log(`as group ${asGroup} -n ${namespace}`);
+  console.log(`--as=${USER} --as-group=${asGroup} -n ${namespace}`);
 
-  function check(item) {
+  function check(item, scope) {
     const type = item.group ? `${item.resource}.${item.group}` : item.resource;
+    const allNamespaces = scope === 'A';
+    const namespaced = scope === 'none' ? false : !allNamespaces && item.namespaced;
 
     for (const verb of item.verbs) {
       const review = {
@@ -115,7 +136,7 @@ function canI(catalog, asGroup, namespace) {
             resource: item.resource,
             subresource: item.subresource,
             verb,
-            namespace: item.namespaced ? namespace : '',
+            namespace: namespaced ? namespace : '',
           },
         },
       };
@@ -136,7 +157,8 @@ function canI(catalog, asGroup, namespace) {
       const allowed = status && status.allowed ? 'yes' : 'no';
       let question = `can-i ${verb} ${type}`;
       if (item.subresource) question += ` --subresource=${item.subresource}`;
-      if (item.namespaced) question += ` -n ${namespace}`;
+      if (allNamespaces) question += ' -A';
+      else if (namespaced) question += ` -n ${namespace}`;
       const line = `${question} = ${allowed}`;
       actual[question] = line;
       actual['can-i'] += `${line}\n`;
@@ -148,5 +170,9 @@ function canI(catalog, asGroup, namespace) {
   for (const group of catalog.groups) {
     for (const item of group.resources) check(item);
   }
+  check({ group: '', resource: '*', subresource: '', namespaced: true, verbs: ['*'] }, 'A');
+  check({ group: '', resource: 'pods', subresource: '', namespaced: true, verbs: ['get'] }, 'A');
+  check({ group: '', resource: 'pods', subresource: '', namespaced: true, verbs: ['*'] });
+  check({ group: 'apps', resource: 'deployments', subresource: '', namespaced: true, verbs: ['*'] });
   return actual;
 }
